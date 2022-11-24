6db4cab38ab304e085e1232c239b756ffd36e25e
Sasi111111 <sasipaimalagari@gmail.com>
2022-11-24 15:31:47 +0530

    new meme website

--- web1.py
+++ web1.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template 
 import requests
+import json
 app = Flask(__name__)
-@app.route('/')
-def function():
-    return "this is new website"
+def meme():
+      url = "https://meme-api.herokuapp.com/gimme"
+      response = requests.get(url).text
+      response1 = json.loads(response)
+      subreddit=response1['subreddit']
+      meme = response1['preview'][0]
+      return subreddit,meme
 
 
-if __name__ == '__main__' :
-    app.run()
+@app.route("/")
+def main():
+    subreddit,meme_pic = meme()
+    return render_template("index.html",meme=meme_pic)
+
+if __name__ =="__main__":
+    app.run(debug=True)
